31838f0a6c5a1c35c90d4f25bab7df984f2b3532
StatTrak <wireless.00.d@gmail.com>
2020-12-11 00:15:39 +0200

    Добавьте файлы проекта.

---
+++ Emblem.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Lab11
+{
+
+    class CEmblem
+    {
+        // Константи  
+        const int DefaultRadius = 50; // Радіус за замовчуванням, пікс  
+
+        // Поля  
+        private Graphics graphics;
+        private int _radius;    // Підтримуюче поле для властивості Radius   
+                                // Властивості  
+        public int X { get; set; }    // Координата X центра кола     
+        public int Y { get; set; }    // Координата Y центра кола  
+        public int Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                _radius = value >= 200 ? 200 : value;
+                _radius = value <= 5 ? 5 : value;
+            }
+        }
+
+        // Конструктор, створює об'єкт елемента (для заданої поверхні   
+        // малювання GDI+) з заданими координатами.      
+        // Радіус кола приймається за замовчуванням     
+        public CEmblem(Graphics graphics, int X, int Y)
+        {
+            this.graphics = graphics;
+            this.X = X;
+            this.Y = Y;
+            this.Radius = DefaultRadius;
+        }
+
+        // Конструктор, створює об'єкт кола (для заданої поверхні      
+        // малювання GDI+) з заданими координатами та радіусом     
+        public CEmblem(Graphics graphics, int X, int Y, int Radius)
+        {
+            this.graphics = graphics;
+            this.X = X;
+            this.Y = Y;
+            this.Radius = Radius;
+        }
+
+        // Малює коло на поверхні малювання GDI+.   
+        // Параметри кола задає перо pen     
+        private void Draw(Pen pen)
+        {
+            Rectangle rectangle = new Rectangle(X - Radius, Y - Radius, 2 * Radius, 2 * Radius);
+            double r = (Radius / 1.15) / Math.Sin(Math.PI / 3);
+            Point p1 = new Point(X, Y - (int)r);
+            Point p2 = new Point(X - (int)(r * Math.Cos(Math.PI / 6)), Y + (int)(r * Math.Sin(Math.PI / 6)));
+            Point p3 = new Point(X + (int)(r * Math.Cos(Math.PI / 6)), Y + (int)(r * Math.Sin(Math.PI / 6)));
+            Point[] triangle = { p1, p2, p3 };
+            graphics.DrawPolygon(pen, triangle);
+            graphics.DrawEllipse(pen, rectangle);
+            graphics.DrawRectangle(pen, rectangle);
+            graphics.DrawPolygon(pen, triangle);
+        }
+
+        // Показує коло (малює на поверхні малювання GDI+ кольором   
+        // переднього плану)     
+        public void Show()
+        {
+            Draw(Pens.Red);
+        }
+
+        // Приховує коло (малює на поверхні малювання GDI+ кольором фону)     
+        public void Hide()
+        {
+            Draw(Pens.White);
+        }
+
+        // Розширює коло: збільшує радіус на один піксель     
+        public void Expand()
+        {
+            Hide();
+            Radius++;
+            Show();
+        }
+
+        // Розширює коло: збільшує радіус на dR пікселів     
+        public void Expand(int dR)
+        {
+
+
+            Hide();
+            Radius = Radius + dR;
+            Show();
+        }
+
+        // Стискає коло: зменшує радіус на один піксель     
+        public void Collapse()
+        {
+            Hide();
+            Radius--;
+            Show();
+        }
+
+        // Стискає коло: зменшує радіус на dR пікселів  
+        public void Collapse(int dR)
+        {
+            Hide();
+            Radius = Radius - dR;
+            Show();
+        }
+
+        // Переміщує коло  
+        public void Move(int dX, int dY)
+        {
+            Hide();
+            X = X + dX;
+            Y = Y + dY;
+            Show();
+        }
+
+    }  // Кінець оголошення класу 
+}
---
+++ fMain.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab11
+{
+    public partial class fMain : Form
+    {
+        CEmblem[] emblems;
+        int EmblemCount = 0;
+        int CurrentEmblemIndex;
+        public fMain()
+        {
+            InitializeComponent();
+            emblems = new CEmblem[100];
+        }
+
+        private void BtnCreateNew_Click(object sender, EventArgs e)
+        {
+            if (EmblemCount >= 99)
+            {
+                MessageBox.Show("Досягнуто межі кількості об'єктів!");
+                return;
+            }
+            Graphics graphics = pnMain.CreateGraphics();
+            CurrentEmblemIndex = EmblemCount;
+
+            // Створення нового об'єкта - екземпляра класу CEmblem     
+            emblems[CurrentEmblemIndex] = new CEmblem(graphics, pnMain.Width / 2, pnMain.Height / 2, 50);
+            emblems[CurrentEmblemIndex].Show();
+            EmblemCount++;
+
+            cbEmblems.Items.Add("Емблема №" + (EmblemCount - 1).ToString());
+            cbEmblems.SelectedIndex = EmblemCount - 1;
+        }
+
+        private void BtnHide_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------     
+            // Приховування поточного об'єкта - екземпляра класу CEmblem  
+            emblems[CurrentEmblemIndex].Hide();
+            // --------------------------------------------------------------- 
+        }
+
+        private void BtnShow_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------  
+            // Відображення поточного об'єкта     
+            emblems[CurrentEmblemIndex].Show();
+            // --------------------------------------------------------------- 
+        }
+
+        private void BtnExpand_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------  
+            // Розширення поточного об'єкта - екземпляра класу CEmblem  
+            emblems[CurrentEmblemIndex].Expand(5);
+            // --------------------------------------------------------------- 
+        }
+
+        private void BtnCollapse_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------     
+            // Стискання поточного об'єкта - екземпляра класу CEmblem     
+            emblems[CurrentEmblemIndex].Collapse(5);
+            // --------------------------------------------------------------- 
+        }
+
+        private void BtnUp_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------  
+            // Переміщення вверх поточного об'єкта     
+            emblems[CurrentEmblemIndex].Move(0, -10);
+            // ---------------------------------------------------------------
+        }
+
+        private void BtnDown_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------  
+            // Переміщення вниз поточного об'єкта     
+            emblems[CurrentEmblemIndex].Move(0, 10);
+            // ---------------------------------------------------------------
+        }
+
+        private void BtnRight_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------  
+            // Переміщення вправо поточного об'єкта     
+            emblems[CurrentEmblemIndex].Move(10, 0);
+            // ---------------------------------------------------------------
+        }
+
+        private void BtnLeft_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------  
+            // Переміщення вліво поточного об'єкта 
+            emblems[CurrentEmblemIndex].Move(-10, 0);
+            // --------------------------------------------------------------- 
+        }
+
+        private void BtnRightFar_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------  
+            // Переміщення поточного об'єкта вправо на більшу відстань     
+            for (int i = 0; i < 100; i++)
+            {
+                emblems[CurrentEmblemIndex].Move(1, 0);
+                System.Threading.Thread.Sleep(5);
+            }
+            // ---------------------------------------------------------------  
+        }
+
+        private void BtnLeftFar_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------  
+            // Переміщення поточного об'єкта вліво на більшу відстань     
+            for (int i = 0; i < 100; i++)
+            {
+                emblems[CurrentEmblemIndex].Move(-1, 0);
+                System.Threading.Thread.Sleep(5);
+            }
+            // --------------------------------------------------------------- 
+        }
+
+        private void BtnUpFar_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------  
+            // Переміщення поточного об'єкта вверх на більшу відстань     
+            for (int i = 0; i < 100; i++)
+            {
+                emblems[CurrentEmblemIndex].Move(0, -
+        1); System.Threading.Thread.Sleep(5);
+            }
+            // ---------------------------------------------------------------
+        }
+
+        private void BtnDownFar_Click(object sender, EventArgs e)
+        {
+            CurrentEmblemIndex = cbEmblems.SelectedIndex;
+            if ((CurrentEmblemIndex > EmblemCount) || (CurrentEmblemIndex < 0))
+                return;
+
+            // ---------------------------------------------------------------  
+            // Переміщення поточного об'єкта вниз на більшу відстань     
+            for (int i = 0; i < 100; i++)
+            {
+                emblems[CurrentEmblemIndex].Move(0, 1);
+                System.Threading.Thread.Sleep(5);
+            }
+            // --------------------------------------------------------------- 
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
